a46d5e4024911b4d227b498e98a981acf0d691a1
StefanStefanov741 <tetijava@gmail.com>
2023-09-07 21:58:22 +0200

    Add project files.

---
+++ Boku_AI/Form1.cs
@@ -0,0 +1,166 @@
+namespace Boku_AI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            BuildGrid();
+        }
+
+        private void BuildGrid() {
+            int hexSize = 80;
+            //Column positions
+            float col1X = 110;
+            float col2X = col1X+58;
+            float col3X = col2X + 58;
+            float col4X = col3X + 58;
+            float col5X = col4X + 58;
+            float col6X = col5X + 58;
+            float col7X = col6X + 58;
+            float col8X = col7X + 58;
+            float col9X = col8X + 58;
+            float col10X = col9X + 58;
+            float col11X = col10X + 58;
+
+            //Row positions
+            float row1Y = 50;
+            float row2Y = row1Y+32.5f;
+            float row3Y = row2Y+32.5f;
+            float row4Y = row3Y+32.5f;
+            float row5Y = row4Y+32.5f;
+            float row6Y = row5Y+32.5f;
+            float row7Y = row6Y+32.5f;
+            float row8Y = row7Y + 32.5f;
+            float row9Y = row8Y + 32.5f;
+            float row10Y = row9Y + 32.5f;
+            float row11Y = row10Y + 32.5f;
+            float row12Y = row11Y + 32.5f;
+            float row13Y = row12Y + 32.5f;
+            float row14Y = row13Y + 32.5f;
+            float row15Y = row14Y + 32.5f;
+            float row16Y = row15Y + 32.5f;
+            float row17Y = row16Y + 32.5f;
+            float row18Y = row17Y + 32.5f;
+            float row19Y = row18Y + 32.5f;
+
+            //Create the board
+
+            //Col1
+            HexFactory(hexSize, col1X, row6Y, "E10");
+            HexFactory(hexSize, col1X, row8Y, "D9");
+            HexFactory(hexSize, col1X, row10Y, "C8");
+            HexFactory(hexSize, col1X, row12Y, "B7");
+            HexFactory(hexSize, col1X, row14Y, "A6");
+            //Col2
+            HexFactory(hexSize, col2X, row5Y, "F10");
+            HexFactory(hexSize, col2X, row7Y, "E9");
+            HexFactory(hexSize, col2X, row9Y, "D8");
+            HexFactory(hexSize, col2X, row11Y, "C7");
+            HexFactory(hexSize, col2X, row13Y, "B6");
+            HexFactory(hexSize, col2X, row15Y, "A5");
+            //Col3
+            HexFactory(hexSize, col3X, row4Y, "G10");
+            HexFactory(hexSize, col3X, row6Y, "F9");
+            HexFactory(hexSize, col3X, row8Y, "E8");
+            HexFactory(hexSize, col3X, row10Y, "D7");
+            HexFactory(hexSize, col3X, row12Y, "C6");
+            HexFactory(hexSize, col3X, row14Y, "B5");
+            HexFactory(hexSize, col3X, row16Y, "A4");
+            //Col 4
+            HexFactory(hexSize, col4X, row3Y, "H10");
+            HexFactory(hexSize, col4X, row5Y, "G9");
+            HexFactory(hexSize, col4X, row7Y, "F8");
+            HexFactory(hexSize, col4X, row9Y, "E7");
+            HexFactory(hexSize, col4X, row11Y, "D6");
+            HexFactory(hexSize, col4X, row13Y, "C5");
+            HexFactory(hexSize, col4X, row15Y, "B4");
+            HexFactory(hexSize, col4X, row17Y, "A3");
+            //Col 5
+            HexFactory(hexSize, col5X, row2Y, "I10");
+            HexFactory(hexSize, col5X, row4Y, "H9");
+            HexFactory(hexSize, col5X, row6Y, "G8");
+            HexFactory(hexSize, col5X, row8Y, "F7");
+            HexFactory(hexSize, col5X, row10Y, "E6");
+            HexFactory(hexSize, col5X, row12Y, "D5");
+            HexFactory(hexSize, col5X, row14Y, "C4");
+            HexFactory(hexSize, col5X, row16Y, "B3");
+            HexFactory(hexSize, col5X, row18Y, "A2");
+            //Col 6 (Middle)
+            HexFactory(hexSize, col6X, row1Y, "J10");
+            HexFactory(hexSize, col6X, row3Y, "I9");
+            HexFactory(hexSize, col6X, row5Y, "H8");
+            HexFactory(hexSize, col6X, row7Y, "G7");
+            HexFactory(hexSize, col6X, row9Y, "F6");
+            HexFactory(hexSize, col6X, row11Y, "E5");
+            HexFactory(hexSize, col6X, row13Y, "D4");
+            HexFactory(hexSize, col6X, row15Y, "C3");
+            HexFactory(hexSize, col6X, row17Y, "B2");
+            HexFactory(hexSize, col6X, row19Y, "A1");
+            //Col 7
+            HexFactory(hexSize, col7X, row2Y, "J9");
+            HexFactory(hexSize, col7X, row4Y, "I8");
+            HexFactory(hexSize, col7X, row6Y, "H7");
+            HexFactory(hexSize, col7X, row8Y, "G6");
+            HexFactory(hexSize, col7X, row10Y, "F5");
+            HexFactory(hexSize, col7X, row12Y, "E4");
+            HexFactory(hexSize, col7X, row14Y, "D3");
+            HexFactory(hexSize, col7X, row16Y, "C2");
+            HexFactory(hexSize, col7X, row18Y, "B1");
+            //Col 8
+            HexFactory(hexSize, col8X, row3Y, "J8");
+            HexFactory(hexSize, col8X, row5Y, "I7");
+            HexFactory(hexSize, col8X, row7Y, "H6");
+            HexFactory(hexSize, col8X, row9Y, "G5");
+            HexFactory(hexSize, col8X, row11Y, "F4");
+            HexFactory(hexSize, col8X, row13Y, "E3");
+            HexFactory(hexSize, col8X, row15Y, "D2");
+            HexFactory(hexSize, col8X, row17Y, "C1");
+            //Col 9
+            HexFactory(hexSize, col9X, row4Y, "J7");
+            HexFactory(hexSize, col9X, row6Y, "I6");
+            HexFactory(hexSize, col9X, row8Y, "H5");
+            HexFactory(hexSize, col9X, row10Y, "G4");
+            HexFactory(hexSize, col9X, row12Y, "F3");
+            HexFactory(hexSize, col9X, row14Y, "E2");
+            HexFactory(hexSize, col9X, row16Y, "D1");
+            //Col 10
+            HexFactory(hexSize, col10X, row5Y, "J6");
+            HexFactory(hexSize, col10X, row7Y, "I5");
+            HexFactory(hexSize, col10X, row9Y, "H4");
+            HexFactory(hexSize, col10X, row11Y, "G3");
+            HexFactory(hexSize, col10X, row13Y, "F2");
+            HexFactory(hexSize, col10X, row15Y, "E1");
+            //Col 11
+            HexFactory(hexSize, col11X, row6Y, "J5");
+            HexFactory(hexSize, col11X, row8Y, "I4");
+            HexFactory(hexSize, col11X, row10Y, "H3");
+            HexFactory(hexSize, col11X, row12Y, "G2");
+            HexFactory(hexSize, col11X, row14Y, "F1");
+        }
+
+        HexagonalButton HexFactory(int size,float posX, float posY, string tag) {
+            //Create button with set parameters
+            HexagonalButton btn = new HexagonalButton(tag);
+            btn.Size = new Size(size, size);
+            btn.Location = new Point((int)posX, (int)posY);
+            btn.Tag = tag;
+            btn.Click += Hex_Click;
+
+            //Add to the form
+            this.Controls.Add(btn);
+
+            //Return btn
+            return btn;
+        }
+
+        private void Hex_Click(object sender, EventArgs e)
+        {
+            //Access the Tag property of the sender to determine which button was clicked
+            string buttonIdentifier = (sender as HexagonalButton).Tag.ToString();
+
+            //Use the button identifier for further action
+            MessageBox.Show($"Hexagone '{buttonIdentifier}' clicked!");
+        }
+    }
+}
---
+++ Boku_AI/HexagonalButton.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+public class HexagonalButton : Control
+{
+    public string tag;
+
+    public HexagonalButton(string t) {
+        this.tag = t;
+        this.TabStop = false;
+    }
+
+    public void clicked() {
+        MessageBox.Show("Clicked: "+tag);
+    }
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        base.OnPaint(e);
+
+        using (Graphics g = e.Graphics)
+        {
+            //Define the points for the hexagon
+            Point[] hexagonPoints = new Point[6];
+            float angle = 360.0f / 6;
+
+            for (int i = 0; i < 6; i++)
+            {
+                float x = (float)(Width / 2 + Width / 2 * Math.Cos(Math.PI * angle * i / 180));
+                float y = (float)(Height / 2 + Height / 2 * Math.Sin(Math.PI * angle * i / 180));
+                hexagonPoints[i] = new Point((int)x, (int)y);
+            }
+
+            //Fill the hexagon
+            using (Brush brush = new SolidBrush(Color.LightBlue))
+            {
+                g.FillPolygon(brush, hexagonPoints);
+            }
+
+            //Draw outline
+            using (Pen pen = new Pen(Color.SlateGray, 3))
+            {
+                g.DrawPolygon(pen, hexagonPoints);
+            }
+
+            //Add Tag as text
+            string buttonText = tag;
+            using (Font font = new Font("Arial", 14, FontStyle.Bold))
+            using (Brush textBrush = new SolidBrush(Color.LimeGreen))
+            {
+                SizeF textSize = g.MeasureString(buttonText, font);
+                PointF textLocation = new PointF((Width - textSize.Width) / 2, (Height - textSize.Height) / 2);
+                g.DrawString(buttonText, font, textBrush, textLocation);
+            }
+        }
+    }
+    protected override void OnResize(EventArgs e)
+    {
+        base.OnResize(e);
+
+        // Recreate the region to match the new size
+        using (GraphicsPath path = new GraphicsPath())
+        {
+            Point[] hexagonPoints = new Point[6];
+            float angle = 360.0f / 6;
+
+            for (int i = 0; i < 6; i++)
+            {
+                float x = (float)(Width / 2 + Width / 2 * Math.Cos(Math.PI * angle * i / 180));
+                float y = (float)(Height / 2 + Height / 2 * Math.Sin(Math.PI * angle * i / 180));
+                hexagonPoints[i] = new Point((int)x, (int)y);
+            }
+
+            path.AddPolygon(hexagonPoints);
+
+            // Set the region to the hexagon shape
+            this.Region = new Region(path);
+        }
+    }
+}
